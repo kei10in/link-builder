8318fa35a7a64c2b1a5c87b0625ea2efc890e24f
Keisuke Ijuin <kei10in@gmail.com>
2023-02-04 00:18:04 +0900

    Introduce storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
